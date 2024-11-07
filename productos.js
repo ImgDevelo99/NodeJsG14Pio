a5e1668fd3b960b428dba53d56df2bb00a208db9
Diego G <ferchod2010@gmail.com>
2024-11-06 21:06:50 -0500

    modulos

--- productos.js
+++ productos.js
@@ -9,7 +9,6 @@ function agregarProducto(nombre, precio){
 function listarProducto() {
     return inventario.length ? inventario: "no hay productos en el inventario";
 }
-
 // function listarProducto() {
 //     if(inventario.length > 0){
 //         return inventario;
@@ -17,3 +16,14 @@ function listarProducto() {
 //         return "no hay productos en el inventario";
 //     }
 // }
+function eliminarProducto(nombre){
+    //busca el indice del producto en el arreglo inventario
+    const index = inventario.findIndex((producto) => producto.nombre === nombre);
+    if(index !== -1){//verifica si se encontro el producto (si el indice es diferente a -1)
+        inventario.splice(index, 1);//si el producto existelo elimina inventario
+        return `Producto ${nombre} eliminado con exito`;
+    }
+    return `producto ${nombre} no encontrado`;
+}
+//exportar las funciones
+module.exports = {agregarProducto, eliminarProducto, listarProducto};
