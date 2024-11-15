0f38d01734a548b71ff4cc0ec393fe120ce3bd01
Diego G <ferchod2010@gmail.com>
2024-11-14 19:49:39 -0500

    ejercicio web

--- servidor_web/server.js
+++ servidor_web/server.js
@@ -29,16 +29,46 @@ const PORT = 3000;
 //crear el servidor http
 const server = http.createServer((req, res) => {
     //si la solicitud es para la ruta principal / y el metodo es get
-    if(req.url === "/" && req.method === 'GET')// manejo ruta principal
+    if(req.url === "/" && req.method === 'GET'){// manejo ruta principal
+
+//leer el archivo formulario.html de forma asincrona
 fs.readFile('formulario.html', (err,data) => { //lectura y envio del archivo
     if(err){
         res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end("error interno del servidor");
+        res.end("error 500 interno del servidor");
     }else{
         res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end(data)
+        res.end(data);
     }
 });
 
-})
+//si la solicitud es un POST a la ruta /submit procesamos los datos enviados por el formulario
+}else if (req.url === '/submit' && req.method === 'POST'){
+
+    let body = '';
+    //escucha el evento 'data' para acumular los datos entrantes
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+
+    req.on('end',() => {
+        console.log('datos recibidos', body);
+
+         //responder con un mensaje de exito
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end('el formulario se envio correctamente');
+    });
+    
+
+}else{
+    res.writeHead(404, {'Content-type': 'text/plain'});
+    res.end(" Error 404 | Not Found ");
+}
+});
+// el servidor escuchara el pueto especifico
+server.listen(PORT, () => {
+    console.log(`servidor en ejecucion en http://localhost:${PORT}`);
+});
+
+
 
